1020d1172a8d9be4687cf39c03f780adec6894d4
hackysphere <hi@hcks.dev>
2025-12-16 23:57:53 -0600

    fix tabs

--- dots/.config/nvim/lua/config/options.lua
+++ dots/.config/nvim/lua/config/options.lua
@@ -1,8 +1,11 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
--- vim.opt.tabstop = 4 -- this is not recommended, but I might enable it if it becomes annoying to not use 
-vim.opt.shiftwidth = 4
-vim.opt.autoindent = true
+
+vim.opt.tabstop = 4 -- amount of spaces that a <tab> will be
+vim.opt.shiftwidth = 4 -- how much to indent by; should always be the same as tabstop
+vim.opt.autoindent = true -- autoindent new lines if the previous was indented
+vim.opt.expandtab = false -- use tabs when pressing <tab> instead of spaces
+
 vim.opt.mouse = 'a'
 vim.cmd.colorscheme('sorbet')
 vim.opt.smartcase = true -- case-insensitive search when all lowercase, case-sensitive when not all lowercase
