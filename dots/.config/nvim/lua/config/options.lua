5c98188a4bb15ebb7dc73e6391e9446d519da8f5
hackysphere <hi@hcks.dev>
2026-03-24 13:44:12 -0600

    nvim: change most options to use the vim.o format

--- dots/.config/nvim/lua/config/options.lua
+++ dots/.config/nvim/lua/config/options.lua
@@ -1,33 +1,33 @@
-vim.opt.number = true -- show numbers on side
-vim.opt.relativenumber = true -- show relative numbers
-vim.opt.cursorline = true -- highlight current line number
+vim.o.number = true -- show numbers on side
+vim.o.relativenumber = true -- show relative numbers
+vim.o.cursorline = true -- highlight current line number
 
-vim.opt.tabstop = 4 -- amount of spaces that a <tab> will be
-vim.opt.shiftwidth = 4 -- how much to indent by; should always be the same as tabstop
-vim.opt.autoindent = true -- autoindent new lines if the previous was indented
-vim.opt.expandtab = false -- use tabs when pressing <tab> instead of spaces
-vim.opt.breakindent = true -- keep indent spacing when wrapping lines
+vim.o.tabstop = 4 -- amount of spaces that a <tab> will be
+vim.o.shiftwidth = 4 -- how much to indent by; should always be the same as tabstop
+vim.o.autoindent = true -- autoindent new lines if the previous was indented
+vim.o.expandtab = false -- use tabs when pressing <tab> instead of spaces
+vim.o.breakindent = true -- keep indent spacing when wrapping lines
 
-vim.opt.list = true -- shows leading 'invisible characters'; config is below
+vim.o.list = true -- shows leading 'invisible characters'; config is below
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
-vim.opt.splitbelow = true -- Split down by default when using split
-vim.opt.splitright = true -- Split to the right by default when using vsplit
+vim.o.splitbelow = true -- Split down by default when using split
+vim.o.splitright = true -- Split to the right by default when using vsplit
 
 -- spellcheck
-vim.opt.spelllang = 'en_us'
-vim.opt.spell = true
+vim.o.spelllang = 'en_us'
+vim.o.spell = true
 
-vim.opt.mouse = 'a' -- enable mouse *everywhere*
+vim.o.mouse = 'a' -- enable mouse *everywhere*
 vim.cmd.colorscheme('sorbet')
-vim.opt.ignorecase = true -- ignore case in searches and substitutions; required for smartcase to function
-vim.opt.smartcase = true -- case-insensitive search when all lowercase, case-sensitive when not all lowercase
-vim.opt.undofile = true -- allows for undo history over multiple sessions
-vim.opt.inccommand = "nosplit" -- shows substitutions while you are typing them
-vim.opt.confirm = true -- ask for confirmation when quitting an unsaved file
-vim.opt.scrolloff = 5 -- keep 5 lines visible at top and bottom when scrolling
-vim.opt.showmode = false -- don't show current "mode", as statusline already shows it
-vim.opt.updatetime = 2000 -- time for neovim to automatically save to swap and for CursorHold (which also controls LSP symbol highlights)
+vim.o.ignorecase = true -- ignore case in searches and substitutions; required for smartcase to function
+vim.o.smartcase = true -- case-insensitive search when all lowercase, case-sensitive when not all lowercase
+vim.o.undofile = true -- allows for undo history over multiple sessions
+vim.o.inccommand = "nosplit" -- shows substitutions while you are typing them
+vim.o.confirm = true -- ask for confirmation when quitting an unsaved file
+vim.o.scrolloff = 5 -- keep 5 lines visible at top and bottom when scrolling
+vim.o.showmode = false -- don't show current "mode", as statusline already shows it
+vim.o.updatetime = 2000 -- time for neovim to automatically save to swap and for CursorHold (which also controls LSP symbol highlights)
 
 vim.g.mapleader = " " -- key used for custom binds
 vim.g.maplocalleader = "\\" -- key used for *local* custom binds (like in lazy.nvim)
