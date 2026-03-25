4119ed013f9bb0242d80a3c68f304b66413dcda3
hackysphere <hi@hcks.dev>
2026-03-25 01:11:56 -0600

    nvim: change some keymaps (notably debugger ones)

--- dots/.config/nvim/lua/config/keymaps.lua
+++ dots/.config/nvim/lua/config/keymaps.lua
@@ -8,7 +8,7 @@ end
 local function telescopeMaps()
 	local builtin = require("telescope.builtin")
 	vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find [f]iles" })
-	vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live [g]rep" }) -- finds in current directory files
+	vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live [g]rep" })
 	vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope [b]uffers" })
 	vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope [h]elp tags" })
 	vim.keymap.set(
@@ -32,23 +32,20 @@ end
 local function snacksMaps()
 	vim.keymap.set("n", "<leader>t", Snacks.terminal.open, { desc = "Open [t]erminal" })
 	vim.keymap.set("n", "<leader>og", Snacks.lazygit.open, { desc = "Open Lazy[g]it" })
-
-	vim.keymap.set("n", "<leader>t", Snacks.terminal.open, { desc = "Open [t]erminal" })
-	vim.keymap.set("n", "<leader>og", Snacks.lazygit.open, { desc = "Open Lazy[g]it" })
 	vim.keymap.set("n", "<leader>oe", Snacks.explorer.open, { desc = "Open file [e]xplorer" })
 
-	vim.keymap.set("n", "<leader>gi", function()
-		Snacks.picker.gh_issue()
-	end, { desc = "GitHub [i]ssues" })
-	vim.keymap.set("n", "<leader>gI", function()
-		Snacks.picker.gh_issue({ state = "all" })
-	end, { desc = "GitHub [i]ssues (all)" })
-	vim.keymap.set("n", "<leader>gp", function()
-		Snacks.picker.gh_pr()
-	end, { desc = "GitHub [p]ull requests" })
-	vim.keymap.set("n", "<leader>gP", function()
-		Snacks.picker.gh_pr({ state = "all" })
-	end, { desc = "GitHub [p]ull requests (all)" })
+	-- vim.keymap.set("n", "<leader>gi", function()
+	-- 	Snacks.picker.gh_issue()
+	-- end, { desc = "GitHub [i]ssues" })
+	-- vim.keymap.set("n", "<leader>gI", function()
+	-- 	Snacks.picker.gh_issue({ state = "all" })
+	-- end, { desc = "GitHub [i]ssues (all)" })
+	-- vim.keymap.set("n", "<leader>gp", function()
+	-- 	Snacks.picker.gh_pr()
+	-- end, { desc = "GitHub [p]ull requests" })
+	-- vim.keymap.set("n", "<leader>gP", function()
+	-- 	Snacks.picker.gh_pr({ state = "all" })
+	-- end, { desc = "GitHub [p]ull requests (all)" })
 
 	vim.keymap.set("n", "<leader>u", Snacks.picker.undo, { desc = "[U]ndo history" })
 end
@@ -69,9 +66,9 @@ local function lspMaps()
 
 	-- nvim default keymaps:
 	--- gra: run a code action (on diagnostic)
-	--- gri: go to implementation
 	--- grn: rename symbol
 	--- grr: go to reference(s)
+	--- gri: go to implementation
 	--- grt: go to type definition
 	--- gO: list all document symbols
 	--- [insert] Ctrl-s: signature help (shows documentation, unlike blink.cmp (Ctrl-k))
@@ -81,7 +78,8 @@ local function diagnosticMaps()
 	vim.keymap.set("n", "<leader>ds", vim.diagnostic.open_float, { desc = "[S]how diagnostic" })
 	vim.keymap.set("n", "<leader>dt", function()
 		vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text })
-	end, { desc = "Toggle diagnostic messages" })
+	end, { desc = "[T]oggle diagnostic messages" })
+	-- vim.keymap.set("n", "<leader>dt", function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end, { desc = "[T]oggle diagnostics" })
 end
 
 local function windowMaps()
@@ -101,29 +99,33 @@ local function sessionMaps()
 end
 
 local function dapMaps()
+	-- Session
+	-- TODO: reconsider binds
 	vim.keymap.set("n", "<leader>bn", function()
 		require("dap").continue({ new = true })
 	end, { desc = "[N]ew session (DAP)" })
-	vim.keymap.set("n", "<leader>bt", function()
+	vim.keymap.set("n", "<leader>bs", function()
 		require("dap").terminate()
-	end, { desc = "[T]erminate session (DAP)" })
-	vim.keymap.set("n", "<leader>bx", function()
-		require("dap").disconnect()
-	end, { desc = "Disconnect session (DAP)" })
+	end, { desc = "[S]top session (DAP)" })
 
-	vim.keymap.set("n", "<leader>bb", function()
+	-- Breakpoints
+	vim.keymap.set("n", "<F9>", function()
 		require("dap").toggle_breakpoint()
-	end, { desc = "Toggle [b]reakpoint (DAP)" })
-	vim.keymap.set("n", "<leader>bc", function()
+	end, { desc = "Toggle breakpoint (DAP)" })
+	-- a CTRLd FN key is the same as FN+24 (at least in kitty)
+	-- using CTRL because it's a "controlled" breakpoint
+	vim.keymap.set("n", "<F33>", function()
 		require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
 	end, { desc = "Set [c]onditional breakpoint (DAP)" })
-	vim.keymap.set("n", "<leader>bl", function()
+	-- a shifted FN key is the same as FN+12 (at least in kitty, it is)
+	vim.keymap.set("n", "<F21>", function()
 		require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
 	end, { desc = "Set [l]ogpoint (DAP)" })
 	vim.keymap.set("n", "<leader>bd", function()
 		require("dap").clear_breakpoints()
 	end, { desc = "[D]elete all breakpoints (DAP)" })
 
+	-- Stepping
 	vim.keymap.set("n", "<F5>", function()
 		require("dap").continue()
 	end, { desc = "[C]ontinue (DAP)" })
@@ -133,10 +135,11 @@ local function dapMaps()
 	vim.keymap.set("n", "<F11>", function()
 		require("dap").step_into()
 	end, { desc = "Step [i]nto (DAP)" })
-	vim.keymap.set("n", "<F12>", function()
+	vim.keymap.set("n", "<F12>", function() -- TODO: change to shift+f11
 		require("dap").step_out()
 	end, { desc = "Step o[u]t (DAP)" })
 
+	-- Widgets
 	vim.keymap.set({ "n", "v" }, "<leader>bh", function()
 		require("dap.ui.widgets").hover()
 	end, { desc = "[H]over (DAP)" })
@@ -151,9 +154,9 @@ end
 -- these are keymaps that are either non-plugin-dependent, only one is necessary for a plugin, or really important
 vim.keymap.set("n", "<leader>ol", ":Lazy<CR>", { desc = "Open [L]azy.nvim" })
 vim.keymap.set("n", "<leader>om", ":Mason<CR>", { desc = "Open [M]ason" })
-vim.keymap.set("n", "<leader>rf", function()
+vim.keymap.set("n", "<leader>m", function()
 	require("conform").format()
-end, { desc = "Run [f]ormatter" })
+end, { desc = "For[m]at" })
 vim.keymap.set("n", "<leader>p", ":Markview splitToggle<CR>", { desc = "[P]review file" })
 vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>") -- unhighlights searches after pressing Esc, but still highlights future search requests
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
